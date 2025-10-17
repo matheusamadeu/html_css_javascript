3b046bf97e16d293aa2728fd24eafdd670ad0a3f
matheusamadeu <00001099049908sp@al.educacao.sp.gov.br>
2025-10-17 12:04:39 -0300

    criamos umas listas dos clientes

--- 02_aula_funcoes_e_eventos/asset/JS/app.js
+++ 02_aula_funcoes_e_eventos/asset/JS/app.js
@@ -1,24 +1,45 @@
-const nome = document.querySelector("#nome");
-const email = document.querySelector("#email");
-const telefone = document.querySelector("#telefone");
-const nascimento = document.querySelector("#nascimento");
-const btn_salvar = document.querySelector("#btn-salvar")
+const Nome = document.querySelector("#nome")
+const Email = document.querySelector("#email")
+const Telefone = document.querySelector("#telefone")
+const Nascimento = document.querySelector("#nascimento")
+const Btn_Salvar = document.querySelector("#btn-salvar")
+const Resultado = document.querySelector("#resultado")
+const db_dados = localStorage.getItem("Clientes") ? JSON.parse(localStorage.getItem("Clientes")) : []
 
-const nomeValor = nome.Value
-const emailValor = email.Value
-const telefoneValor = telefone.Value
-const nascimentoValor = nascimento.Value
+const nome_Valor = Nome.value
+const email_Valor = Email.value
+const telefone_Valor = Telefone.value
+const nascimento_Valor = Nascimento.value
 
-btn_salvar.addEventListener("click", (e) => {
-    e.preventDefault
-    
-console.log(nomeValor)
-console.log(emailValor)
-console.log(telefoneValor)
-console.log(nascimentoValor)  
+Btn_Salvar.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (!Nome.value || !Email.value || !Telefone.value || !Nascimento.value) {
+        return console.log("Os Campos Não podem ficar vazios");
+    };
+
+    const dados = {
+        nome: Nome.value,
+        email: Email.value,
+        telefone: Telefone.value,
+        nascimento: Nascimento.value,
+    };
+    db_dados.push(dados)
+    console.log(db_dados);
+    localStorage.setItem("Clientes", JSON.stringify(db_dados))
+
+    Nome.value = "";
+    Email.value = "";
+    Telefone.value = "";
+    Nascimento.value = "";
 });
 
+Nome.addEventListener("input", () => {
+    if (Nome.value.length < 2) {
+        return console.log("Nome precisa ter mais de 2 caracteres");
+    };
+});
 
-nome .addEventListener("input", () => {});
-email .addEventListener("input", () => {});
-telefone .addEventListener("input", () => {});
+Email.addEventListener("input", () => { });
+
+Telefone.addEventListener("input", () => { });
